82b8cc836b177382b492cd29ecf7b81a84d5b8af
Julian Gamez <julian.e.gamez@gds.ey.com>
2019-04-12 12:45:09 -0300

    first commit

---
+++ StorageAccountPOC/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAccountPOC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new SasProvider();
+            var storageAccountName = ConfigurationManager.AppSettings["SiteStorageAccountName"];
+            var sasToken = provider.SASToken;
+
+            StorageCredentials accountSAS = new StorageCredentials(sasToken);
+            CloudStorageAccount accountWithSAS = new CloudStorageAccount(accountSAS, storageAccountName, endpointSuffix: null, useHttps: true);
+            var tableClient = accountWithSAS.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("Users");
+            table.CreateIfNotExists();
+
+            var query = new TableQuery();
+            var results = table.ExecuteQuery(query);
+
+            Console.WriteLine(JsonConvert.SerializeObject(results));
+
+            var tableEntity = new TableEntity("Users", "{\"name\":\"Antonio\", \"lastName\":\"Briones\"}");
+            var op = TableOperation.InsertOrReplace(tableEntity);
+            table.Execute(op);
+
+            op = TableOperation.Delete(tableEntity);
+            table.Execute(op);
+        }
+    }
+}
---
+++ StorageAccountPOC/SasProvider.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAccountPOC
+{
+    public class SasProvider
+    {
+        private string token;
+        public string SASToken
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.token))
+                {
+                    this.token = this.GetAccountSASToken();
+                }
+
+                return this.token;
+            }
+        }
+
+        public SasProvider()
+        {
+
+        }
+
+        private string GetAccountSASToken()
+        {
+            var ConnectionString = ConfigurationManager.AppSettings["StorageAccountConnectionString"];
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            SharedAccessAccountPolicy policy = new SharedAccessAccountPolicy()
+            {
+                Permissions = SharedAccessAccountPermissions.Read |
+                                SharedAccessAccountPermissions.Create |
+                                SharedAccessAccountPermissions.Write |
+                                SharedAccessAccountPermissions.List |
+                                SharedAccessAccountPermissions.Add |
+                                SharedAccessAccountPermissions.Delete |
+                                SharedAccessAccountPermissions.Update,
+                Services = SharedAccessAccountServices.Table,
+                ResourceTypes = SharedAccessAccountResourceTypes.Object | SharedAccessAccountResourceTypes.Container | SharedAccessAccountResourceTypes.Service,
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                Protocols = SharedAccessProtocol.HttpsOnly
+            };
+
+            return storageAccount.GetSharedAccessSignature(policy);
+        }
+    }
+}
